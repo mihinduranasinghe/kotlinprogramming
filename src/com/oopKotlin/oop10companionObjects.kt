ea908d7187f0b0492d97e184b3b35c327203ff7a
Mihindu Ranasinghe <37303081+MihinduRanasinghe@users.noreply.github.com>
2019-10-01 17:44:21 +0530

    Adding OOP companion object files

---
+++ src/com/oopKotlin/oop10companionObjects.kt
@@ -0,0 +1,41 @@
+package com.oopKotlin
+
+//COMPANION OBJECTS ARE SAME AS OBJECTS  BUT DECLARED WITHIN A CLASS
+//BEHAVE LIKE STATIC
+//previous one , inside a class.(Thats the only thing in companion object with the eyword of companion before object keyword)
+
+fun main(args : Array<String>){
+    //ar customersData = customersData()
+    customersData.count=5
+    var output= customersData.typeOfCustomers()
+    println(output)
+
+    customersData.myMethod("Hellow")
+
+}
+
+class myclass{
+
+    companion object customersData{//behave like static class
+    var count: Int = -1 //behave like static variable
+        fun typeOfCustomers(): String{//behave like static method
+            return "Srilankan"
+        }
+        override fun myMethod(str:String){//currently behving like static method
+            //super.myMethod(str)
+            println("object customer data $str")
+
+        }
+
+        init {
+            //we can initialize anything also
+        }
+
+
+    }
+
+
+}
+
+
+
